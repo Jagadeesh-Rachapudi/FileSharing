dbf037752b0dc8b8c588f4d3817b8487a444c27c
Jagadeesh Rachapudi <rachapudijagadeesh9580@gmail.com>
2024-04-24 12:14:16 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import SampleComponent from './chat/chat';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
 
 function App() {
+  const [pdfFile, setPdfFile] = useState(null);
+  const [disableButton, setDisableButton] = useState(true);
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      if (pdfFile) {
+        try {
+          const formData = new FormData();
+          formData.append('pdfFile', pdfFile);
+          const response = await fetch('http://10.8.82.221:5000/upload', {
+            method: 'POST',
+            body: formData,
+          });
+    
+          if (response.ok) {
+            console.log('File uploaded successfully');
+          } else {
+            console.error('Failed to upload file');
+          }
+        } catch (error) {
+          console.error('Error uploading file:', error);
+        
+        }
+      } else {
+        console.log('No file selected');
+        
+      }
+    };    
+  
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setPdfFile(file);
+    setDisableButton(false)
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form onSubmit={handleSubmit}>
+        <div className='Heading' >Submit PDF</div>
+        <input id="pdfFile" className="pdfForm" type="file" accept="application/pdf" onChange={handleFileChange} />
+        <Button variant="secondary" className='SubmitPDFButton' disabled={disableButton} onClick={handleSubmit} >Submit</Button>  
+      </form>
       </header>
     </div>
   );
---
+++ src/chat/chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SampleComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample JSX file.</p>
+    </div>
+  );
+};
+
+export default SampleComponent;
